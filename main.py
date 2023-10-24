21e40dcbef5f7fbdec82304baced1a17dfea101c
ProMeX04 <laihieu2714@gmail.com>
2023-10-24 12:34:23 +0700

    Add comment

--- main.py
+++ main.py
@@ -2,36 +2,39 @@ import requests
 import subprocess
 from bs4 import BeautifulSoup
 from tkinter import *
-#  Danh sách các buổi
+
+# Danh sách các buổi
 list_lesson = (2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
                15, 16, 17, 18, 19, 20, 21,22, 23, 24, 25, 26)
-#  Số lượng bài tập
+# Số lượng bài tập
 num_exercise = (29, 20, 27, 0, 7, 17, 0, 6, 26, 13, 5, 10,
                 11, 0, 20, 5, 6, 10, 0, 6, 0, 0, 11, 0, 5, 258)
 
-session_id = 'dfpgfnnkqsknb6ur16441oa6yq90hr32	'
+# Session ID của bạn (cần được cung cấp)
+session_id = 'dfpgfnnkqsknb6ur16441oa6yq90hr32'
+
+# Đường dẫn lưu trữ dữ liệu dưới dạng CSV
 path = "BXH.csv"
 
-#  Nhóm người cần check
+# Danh sách các thành viên cần kiểm tra (có thể thay đổi mặc định theo nhóm)
 Group = ["phanhhoccode", "nguyenvanchinh28", "anhkhoi16", "Nguyenduytan"]
+
 def UI():
     window = Tk()
     window.geometry("500x100")
     window.title("Lấy dữ liệu bài tập") 
-    label = Label(window , text= "Nhập ID hoặc danh sách ID cần kiểm tra", font="intelOne")
+    label = Label(window , text= "Nhập ID hoặc danh sách ID cần kiểm tra cách nhau bởi dấu cách", font="calibri")
     label.pack(side = 'top')
-    entry_data = StringVar()
-    entry = Entry(window , width = 100 , foreground="green", textvariable=entry_data)
+    entry = Entry(window , width = 100 , foreground="green")
     entry.pack(side = TOP)
     def get():
         global Group
         if(entry.get().split()):
             Group = entry.get().split()
         window.destroy()
-    button = Button(window,command=get,text="Gửi")
+    button = Button(window,command=get,text="Collect", font="calibri")
     button.pack(side=TOP)
     window.mainloop()
-   
 
 def request(session_id) -> None:
     session = requests.Session()
@@ -87,7 +90,8 @@ def WriteToCSV(path, Group, User) -> None:
                 OutFile.write("\n")
 
 if __name__ == "__main__":
-    UI()
+    # bật / tắt giao diện nhập
+    # UI()
     res = request(session_id)
     if res.status_code == 200:
         print("Success")
