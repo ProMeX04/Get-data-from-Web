d07ef9c3551a3c7fd8c4e820791290b7e3cd1dfa
ProMeX04 <148797127+ProMeX04@users.noreply.github.com>
2023-10-31 07:18:37 +0000

    Import Docstrings

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-# Cài đặt python và Cài đặt các thư viện phía dưới mới có thể sử dụng được
-import requests
+"""Cài đặt python và Cài đặt các thư viện phía dưới mới có thể sử dụng được"""
 import subprocess
-from bs4 import BeautifulSoup
 from tkinter import *
-
+import requests
+from bs4 import BeautifulSoup
 # Danh sách các buổi
 list_lesson = (2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
-               15, 16, 17, 18, 19, 20, 21,22, 23, 24, 25, 26)
+               15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26)
 # Số lượng bài tập
 num_exercise = (29, 20, 27, 0, 7, 17, 0, 6, 26, 13, 5, 10,
                 19, 0, 20, 6, 6, 10, 0, 6, 0, 0, 11, 0, 5, 258)
@@ -15,39 +14,56 @@ num_exercise = (29, 20, 27, 0, 7, 17, 0, 6, 26, 13, 5, 10,
 session_id = 'dfpgfnnkqsknb6ur16441oa6yq90hr32'
 
 # Đường dẫn lưu trữ dữ liệu dưới dạng CSV
-path = "BXH.csv"
+__path__ = "BXH.csv"
 
 # Danh sách các thành viên cần kiểm tra (có thể thay đổi mặc định theo nhóm)
-Group = ["alisegaunt","Hoanghieu2603","","","kienbnvn2004","","","",
-         "","","","","","","nguyenvanchinh28", "Nguyenduytan", "phanhhoccode", "anhkhoi16","",
-         "Tram_le2003","huynhduong152","","QuocThang","nguyenduchuong300903fullhouse","","nhi9999","",
-         "gia_huy922004","daoduykhai77","HoanganhIT2K4","anhquan","","",
-         "Phuong_10","Huongphanminh","giaudeptrai","Ducthinh20","ninhlong21","duongngan1810","duongngan1810"]
+group = ["alisegaunt", "Hoanghieu2603", "", "", "kienbnvn2004", "", "", "",
+         "", "", "", "", "", "", "nguyenvanchinh28", "Nguyenduytan",
+         "phanhhoccode", "anhkhoi16", "",
+         "Tram_le2003", "huynhduong152", "", "QuocThang",
+         "nguyenduchuong300903fullhouse", "", "nhi9999", "",
+         "gia_huy922004", "daoduykhai77", "HoanganhIT2K4",
+         "anhquan", "", "",
+         "Phuong_10", "Huongphanminh", "giaudeptrai", "Ducthinh20",
+         "ninhlong21", "duongngan1810", "duongngan1810"]
 
-def UI():
+
+def user_interface():
+    """function show windows for user"""
     window = Tk()
     window.geometry("500x100")
-    window.title("Lấy dữ liệu bài tập") 
-    label = Label(window , text= "Nhập ID hoặc danh sách ID cần kiểm tra cách nhau bởi dấu cách", font="calibri")
-    label.pack(side = 'top')
-    entry = Entry(window , width = 100 , foreground="green")
-    entry.pack(side = TOP)
+    window.title("Lấy dữ liệu bài tập")
+    label = Label(window, text="Nhập ID hoặc danh sách ID cần kiểm tra cách nhau bởi dấu cách", 
+                font="calibri")
+    label.pack(side='top')
+    entry = Entry(window, width=100, foreground="green")
+    entry.pack(side=TOP)
+
     def get():
-        global Group
-        if(entry.get().split()):
-            Group = entry.get().split()
+        if (entry.get().split()):
+            group = entry.get().split()
         window.destroy()
-    button = Button(window,command=get,text="Collect", font="calibri")
+    button = Button(window, command=get, text="Collect", font="calibri")
     button.pack(side=TOP)
     window.mainloop()
 
+
 def request(session_id) -> None:
+    """_summary_
+
+    Args:
+        session_id (_string_): _session id from web_
+
+    Returns:
+        _type_: _string_
+    """
     session = requests.Session()
     session.cookies.set('sessionid', session_id)
     response = session.get("https://laptrinh24h.vn/contest/cpp33/ranking/")
     return response
 
-def GetData(response) -> list:
+
+def get_data(response) -> list:
     res = response.text
     soup = BeautifulSoup(res, 'html.parser')
     table = soup.find('table')
@@ -70,21 +86,22 @@ def GetData(response) -> list:
                             bt = href.split(
                                 '/')[-2].rstrip("cpp").split('b')
                             if len(bt) == 2:
-                                if (anchor.text[:2] == '10'):
+                                if anchor.text[:2] == '10':
                                     User[index_row][bt[1]] += 1
                                     cnt += 1
-            if (index_row > -1):
+            if index_row > -1:
                 User[index_row]["Total"] = cnt
     return User
 
-def WriteToCSV(path, Group, User) -> None:
-    with open(path, "w", encoding="utf-8") as OutFile:
+
+def write_to_csv(__path__, group, User) -> None:
+    with open(__path__, "w", encoding="utf-8") as OutFile:
         OutFile.write("Rank,ID,")
         for i in list_lesson:
             OutFile.write("Lesson "+str(i)+",")
         OutFile.write("Total,")
         OutFile.write("\n")
-        for ID in Group:
+        for ID in group:
             for x in User:
                 if x["ID"] == ID:
                     for index, j in enumerate(x):
@@ -95,14 +112,15 @@ def WriteToCSV(path, Group, User) -> None:
                         OutFile.write(',')
             OutFile.write("\n")
 
+
 if __name__ == "__main__":
     # bật / tắt giao diện nhập
-    # UI()
+    user_interface()
     res = request(session_id)
     if res.status_code == 200:
         print("Success")
-        data = GetData(res)
-        WriteToCSV(path,Group, data)
-        subprocess.Popen(['start', path], shell=True)
+        data = get_data(res)
+        write_to_csv(__path__, group, data)
+        subprocess.Popen(['cat', __path__], shell=False)
     else:
         print("Can't connect")
