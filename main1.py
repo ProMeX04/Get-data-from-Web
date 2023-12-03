052a36385ee86fb815a47b364ba0a602ecbbf576
ProMeX04 <laihieu2714@gmail.com>
2023-12-03 18:33:39 +0700

    Remove unnecessary files and update code formatting

--- main1.py
+++ main1.py
@@ -3,27 +3,46 @@ import subprocess
 import requests
 import pandas as pd
 from io import StringIO
-import source
 import openpyxl
 from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl.styles import Border, Side, Font, Alignment, PatternFill
+import subprocess
+
+"""User Data"""
+# credentials
+USERNAME = "laihieu2714"
+PASSWORD = "iiviizedz"
+
+#create list lesson 
+LESSON = (29, 20, 27, 0, 7, 17, 0, 6, 26, 13, 5, 10, 19, 0, 20, 6, 6, 10, 0, 6, 0, 0, 11, 0, 5)
+TOTAL = 258
+CURRENT_LESSON = 26
+
+# class
+CLASS = input("Enter class: ")
+
+# excel path
+EXCEL_PATH = "BXH.xlsx"
+
+# list admin
+LIST_ADMIN = ["laihieu2714", "Tran_Nhi0306","superadmin", "Ninhkfc72", "Nguyen_Thi_Lua_FullHouse"]
 
 
 def login(username, password):
     """Login and retrieve session ID"""
-    login_url = "https://laptrinh24h.vn/accounts/login/?next="  # Replace with your login URL
     session = requests.Session()
-
+    
     # Get CSRF token
-    response = session.get(login_url)
+    response = session.get("https://laptrinh24h.vn/accounts/login/?next=" )
     csrf_token = response.cookies.get("csrftoken")
 
     payload = {
         "username": username,
         "password": password,
-        "csrfmiddlewartoken": csrf_token
+        "csrfmiddlewaretoken": csrf_token
     }
-    session.post(login_url, data=payload)
+    session.post("https://laptrinh24h.vn/accounts/login/?next=" , data=payload)
+    
     session_id = session.cookies.get("sessionid")
     return session_id
 
@@ -31,15 +50,24 @@ def login(username, password):
 def request(url):
     """Get response html web"""
     session = requests.Session()
-    session.cookies['sessionid'] = login("laihieu2714", "iiviizedz")
+    session.cookies['sessionid'] = login(USERNAME, PASSWORD)
     response = session.get(url)
     return response
 
 
 def get_data_frame(response):
-    """_summary_"""
+    """
+    Parses the response text and returns a pandas DataFrame with the extracted data.
+
+    Parameters:
+    response (requests.Response): The response object containing the HTML data.
+
+    Returns:
+    pandas.DataFrame: The DataFrame containing the parsed data.
+    """
     df = pd.read_html(StringIO(response.text))[0]
-    # Read Table from web
+    # Rest of the code...
+    
     df = df.drop(columns=["Organization"])
     df = df.drop(columns=["Points"])
 
@@ -55,23 +83,23 @@ def get_data_frame(response):
         start_lesson, total = 2, 0
         row = [i+1, df.iloc[i, 1].split()[0]]
 
-        if row[1] in source.LIST_ADMIN:
+        if row[1] in LIST_ADMIN:
             continue
 
-        for index, lesson in enumerate(source.LESSON, start=1):
+        for index, lesson in enumerate(LESSON, start=1):
             accepted = (
                 df.iloc[i, start_lesson: start_lesson + lesson] == "10").sum()
             total += accepted
             if accepted:
                 row.append(f"{accepted}/{lesson}")
             else:
-                if index < source.CURRENT_LESSON and lesson != 0:
+                if index < CURRENT_LESSON and lesson != 0:
                     row.append(f"{accepted}/{lesson}")
                 else:
                     row.append("")
 
             start_lesson += lesson
-        row.append(f"{total}/{source.TOTAL}")
+        row.append(f"{total}/{TOTAL}")
         new_df = pd.concat(
             [new_df, pd.DataFrame([row])], ignore_index=True)
 
@@ -133,17 +161,16 @@ def format_cell(df):
                     else:
                         cell.fill = fill_red
 
-    workbook.save("BXH.xlsx")
+    workbook.save(EXCEL_PATH)
 
 
 if __name__ == "__main__":
-    print("Connecting...")
-    res = request(source.URL)
+    res = request("https://laptrinh24h.vn/contest/cpp{}/ranking/".format(CLASS))
     if res.status_code == 200:
-        print("Success")
+        print("Login success")
         dataframe = get_data_frame(res)
         format_cell(dataframe)
-        subprocess.Popen(["start", source.EXCEL], shell=True)
+        subprocess.Popen(["start", EXCEL_PATH], shell=True)
 
     else:
-        print("Can't connect")
+        print("Password or username is wrong")
