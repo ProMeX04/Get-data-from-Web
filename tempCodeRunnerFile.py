b6b182dc84d3515bfa51e6056495bda3068bf948
Lai Hieu <148797127+ProMeX04@users.noreply.github.com>
2023-12-14 03:49:39 +0700

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-        # ttk.Entry(self, textvariable=self.username, font="consolas").grid(row=0, column=1)
-        # ttk.Entry(self, textvariable=self.password, show="*", font="consolas").grid(row=1, column=1)
