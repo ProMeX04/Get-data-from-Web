6dba01c4d5dd041881be673cf89dedf5ae37f2d7
ProMeX04 <laihieu2714@gmail.com>
2023-12-15 21:24:18 +0700

    Update config.ini and main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import subprocess
 import os
-import tkinter as tk
-from tkinter import filedialog, ttk
+from tkinter import Tk as tk,StringVar, IntVar,filedialog, ttk
 import requests
 import pandas as pd
 from io import StringIO
-import openpyxl
+# import openpyxl
+from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl.styles import Border, Side, Font, Alignment, PatternFill
-import configparser
-import time
+from configparser import ConfigParser 
+from time import sleep
 # Constant Variables
 LESSON = (29, 20, 27, 0, 7, 17, 0, 6, 26, 13, 5, 10, 19, 0, 20, 6, 6, 10, 0, 6, 0, 0, 11, 0, 5)
 TOTAL = 258
@@ -37,7 +37,8 @@ class SessionManager:
         """Get response html web"""
         self.login()
         return self.session.get(url)
-
+    
+    
 class DataFrameManager:
     """Manages the DataFrame operations."""
     @staticmethod
@@ -45,37 +46,34 @@ class DataFrameManager:
         """Parses the response text and returns a pandas DataFrame with the extracted data."""
         df = pd.read_html(StringIO(response.text))[0]
         df = df.drop(columns=["Organization", "Points"])
-        for i in range(1, TOTAL-1):
-            head = str(i) + "  10"
-            df[head] = df[head].astype(str).str[0:2]
-        new_df = pd.DataFrame()
+        df.iloc[:, 2:TOTAL+1] = df.iloc[:, 2:TOTAL+1].apply(lambda x: x.astype(str).str[0:2])
+        
+        rows = []
         for i in range(df.shape[0]):
             start_lesson, total = 2, 0
-            row = [i+1, df.iloc[i, 1].split()[0]]
-            if row[1] in LIST_ADMIN:
+            name = df.iloc[i, 1].split()[0]  # Only keep the first part of the name
+            if name in LIST_ADMIN:
                 continue
+            row = [i+1, name]
             for index, lesson in enumerate(LESSON, start=1):
                 accepted = (df.iloc[i, start_lesson: start_lesson + lesson] == "10").sum()
                 total += accepted
-                if accepted:
-                    row.append(f"{accepted}/{lesson}")
-                else:
-                    if index < current_lesson and lesson != 0:
-                        row.append(f"{accepted}/{lesson}")
-                    else:
-                        row.append("")
+                row.append(f"{accepted}/{lesson}" if accepted or (index < current_lesson and lesson != 0) else "")
                 start_lesson += lesson
             row.append(f"{total}/{TOTAL}")
-            new_df = pd.concat([new_df, pd.DataFrame([row])], ignore_index=True)
-        new_df = new_df.rename(columns={0: "Rank", 1: "Name", 27: "Total"})
+            rows.append(row)
+        
+        new_df = pd.DataFrame(rows, columns=["Rank", "Name"] + [f"Lesson {i}" for i in range(1, len(LESSON)+1)] + ["Total"])
         return new_df
+
+
 
 class ExcelManager:
     """Manages the Excel operations."""
     @staticmethod
     def format_cell(dataframe, excel_path):
         """Format cell in excel."""
-        workbook = openpyxl.Workbook()
+        workbook = Workbook()
         sheet = workbook.active
         for row in dataframe_to_rows(dataframe, index=False, header=True):
             sheet.append(row)
@@ -87,20 +85,25 @@ class ExcelManager:
         fill_green = PatternFill(start_color='00FF00', end_color='00FF00', fill_type='solid')
         fill_yellow = PatternFill(start_color='FFFF00', end_color='FFFF00', fill_type='solid')
         fill_red = PatternFill(start_color='FF0000', end_color='FF0000', fill_type='solid')
+
+        def format_cell(cell):
+            cell.border = thin_border
+            cell.font = custom_font
+            cell.alignment = custom_alignment
+            if cell.value and len(str(cell.value).split('/')) == 2:
+                left, right = cell.value.split('/')
+                if int(left) >= int(right):
+                    cell.fill = fill_green
+                elif int(left) >= int(right)//2:
+                    cell.fill = fill_yellow
+                else:
+                    cell.fill = fill_red
+
         for row in sheet.iter_rows():
-            for cell in row:
-                cell.border = thin_border
-                cell.font = custom_font
-                cell.alignment = custom_alignment
-                if cell.value and len(str(cell.value).split('/')) == 2:
-                    left, right = cell.value.split('/')
-                    if int(left) >= int(right):
-                        cell.fill = fill_green
-                    elif int(left) >= int(right)//2:
-                        cell.fill = fill_yellow
-                    else:
-                        cell.fill = fill_red
+            list(map(format_cell, row))
+
         workbook.save(excel_path)
+
 
 class FileManager:
     """Manages the file operations."""
@@ -152,7 +155,7 @@ class ConfigManager:
     @staticmethod
     def save_config(username, password, current_lesson, path, class_name):
         """Saves the username, password, current_lesson, path, and class_name to a config file."""
-        config = configparser.ConfigParser()
+        config = ConfigParser()
         config['DEFAULT'] = {'Username': ConfigManager.encrypt_message(username).decode(), 'Password': ConfigManager.encrypt_message(password).decode(), 'CurrentLesson': current_lesson, 'Path': path, 'Class': class_name}
         with open(FileManager.get_file_path('config.ini'), 'w') as configfile:
             config.write(configfile)
@@ -160,12 +163,12 @@ class ConfigManager:
     @staticmethod
     def load_config():
         """Loads the username, password, current_lesson, path, and class_name from a config file."""
-        config = configparser.ConfigParser()
+        config = ConfigParser()
         config.read(FileManager.get_file_path('config.ini'))
         return ConfigManager.decrypt_message(config['DEFAULT']['Username']), ConfigManager.decrypt_message(config['DEFAULT']['Password']), config['DEFAULT']['CurrentLesson'], config['DEFAULT']['Path'], config['DEFAULT']['Class']
 
 
-class Application(tk.Tk):
+class Application(tk):
     """The main application."""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -177,12 +180,12 @@ class Application(tk.Tk):
         self.iconbitmap(FileManager.get_file_path('ico.ico'))
         self.style = ttk.Style(self)
         self.style.theme_use("vista")
-        self.username = tk.StringVar()
-        self.password = tk.StringVar()
-        self.class_name = tk.StringVar()
-        self.current_lesson = tk.IntVar()
-        self.excel_path = tk.StringVar()
-        self.status = tk.StringVar()
+        self.username = StringVar()
+        self.password = StringVar()
+        self.class_name = StringVar()
+        self.current_lesson = IntVar()
+        self.excel_path = StringVar()
+        self.status = StringVar()
         self.create_widgets()
         
     def focus_next(self, event):
@@ -229,7 +232,7 @@ class Application(tk.Tk):
         for i in range(1, 101):
             self.progress["value"] = i
             self.update_idletasks()
-            time.sleep(0.001)
+            sleep(0.001)
         username = self.username.get()
         password = self.password.get()
         current_lesson = self.current_lesson.get()
